13efa6f4c93af7b608a22ba09f6af4285d3859f3
Asya-Sh <str.injuly@gmail.com>
2024-03-27 11:53:28 +0200

    installation of the project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './index.css';
+import 'modern-normalize';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
